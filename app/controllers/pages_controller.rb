3cc8a5e197580213c8bdba1a615c7708962c6dd9
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-04 08:14:44 -0700

    Added Page model to app/model/

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
     def index
+        @pages = Page.all                       # Page.all is a method to call in the database, store in an instances variable to be access
     end
 end
