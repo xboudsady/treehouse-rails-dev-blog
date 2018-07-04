d109e019f084cd9db5542ebcca1c5f9553b7384e
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-04 10:56:29 -0700

    Updated page_controller.rb to include show(). Updated routes for /pages/:id

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
     def index
         @pages = Page.all                       # Page.all is a method to call in the database, store in an instances variable to be access
+    end                                         # Make sure a model exist, because we are calling Page.all
+    def show
+        @page = Page.find(params[:id])
+        render plain: @page.title
     end
 end
