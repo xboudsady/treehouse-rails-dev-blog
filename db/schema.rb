11129fba144a898e1edc981b5eadfb14fefa0fb8
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-04 08:17:07 -0700

    Migrated Pages model to db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_04_033449) do
+ActiveRecord::Schema.define(version: 2018_07_04_151403) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
