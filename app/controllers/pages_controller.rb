14fd616b0e787f256aea67818bf8da1d22f0ae04
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-04 10:59:23 -0700

    Added elements on for show.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,7 @@ class PagesController < ApplicationController
     def index
         @pages = Page.all                       # Page.all is a method to call in the database, store in an instances variable to be access
     end                                         # Make sure a model exist, because we are calling Page.all
-    def show
-        @page = Page.find(params[:id])
-        render plain: @page.title
+    def show                                    # From the #show routes
+        @page = Page.find(params[:id])          # Pass this data
     end
 end
