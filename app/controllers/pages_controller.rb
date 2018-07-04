91fe381a70de0b5957d45a4e634ddbe0f9c4ed1d
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-04 11:38:42 -0700

    Added New page submission form.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,13 @@ class PagesController < ApplicationController
     def index
         @pages = Page.all                       # Page.all is a method to call in the database, store in an instances variable to be access
     end                                         # Make sure a model exist, because we are calling Page.all
+    
     def show                                    # From the #show routes
         @page = Page.find(params[:id])          # Pass this data
     end
+
+    def new
+        @page = Page.new
+    end
+
 end
