57cdfef5e5b066c8587866a81d159db7ba916486
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-05 10:55:24 -0700

    Added 'belongs_to :post' association to post model, with a 'has_many' association.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,3 @@
 class Comment < ApplicationRecord
+    belongs_to :post                    # Define association to the "post" model. When you set up a model with a "has_many" association on the first mode, you need to set a "belongs_to" to its associated models
 end
